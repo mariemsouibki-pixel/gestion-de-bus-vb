aaec7c9e1f222c70b3dfe5b999e5260f7385b4c4
souibki mariem <mariem.souibki@isgb.ucar.tn>
2025-12-16 20:54:43 +0100

    Add files via upload

---
+++ Reservation.vb
@@ -0,0 +1,28 @@
+﻿Module Reservation
+    Dim nbPlaceDisponible As Integer = 50
+    Public Structure Reservations
+        Dim ligne As String
+        Dim villeDepart As String
+        Dim villeArrivee As String
+        Dim h_debut As String
+        Dim h_fin As String
+        Dim places As Integer
+        Dim Options As List(Of String)
+        Dim type As String
+        Public Sub InitReservation()
+            Options = New List(Of String)
+        End Sub
+    End Structure
+    Public ListeReservation As New List(Of Reservations)
+    Public Function AjouterReservation(r As Reservations)
+        If r.places <= nbPlaceDisponible Then
+            ListeReservation.Add(r)
+            nbPlaceDisponible -= r.places
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+
+End Module
---
+++ bus.vb
@@ -0,0 +1,43 @@
+﻿Public Class Trajetbus
+    Private Sub Trajetbus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnAjouter_Click(sender As Object, e As EventArgs) Handles BtnAjouter.Click
+        ErrorProvider1.Clear()
+        Dim r As Reservations
+        r.Options = New List(Of String)
+        If LstLigne.SelectedIndex = -1 Then
+            ErrorProvider1.SetError(LstLigne, "Veuillez selectionner un ligne!")
+            Return
+        End If
+        If LstVilleDep.SelectedIndex = -1 Then
+            ErrorProvider1.SetError(LstVilleDep, "Veuillez selectionner la ville de depart!")
+            Return
+        End If
+        If LstVilleArrivee.SelectedIndex = -1 Then
+            ErrorProvider1.SetError(LstVilleArrivee, "Veuillez selectionner la ville d'arrivée!")
+            Return
+        End If
+        If LstVilleArrivee.Text = LstVilleDep.Text Then
+            ErrorProvider1.SetError(LstVilleArrivee, " Veuillez selectionner une ville different!")
+            Return
+        End If
+        If DTP_arv.Text = DTP_dep.Text Then
+            ErrorProvider1.SetError(DTP_arv, " Veuillez selectionner deux temps differents!")
+            Return
+        End If
+        If Num_places.Value = 0 Then
+            ErrorProvider1.SetError(Num_places, " Veuillez selectionner au moins une place!")
+            Return
+        End If
+        r.ligne = LstLigne.Text
+        r.villeDepart = LstVilleDep.Text
+        r.villeArrivee = LstVilleArrivee.Text
+        r.h_debut = DTP_dep.Text
+        r.h_fin = DTP_arv.Text
+        r.places = Num_places.Value
+        AjouterReservation(r)
+        InitReservation()
+    End Sub
+End Class
